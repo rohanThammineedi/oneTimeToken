14a49a9cddad8e44fde27977b08a1f2ee4b15a1d
rohan8888 <164673572+rohanThammineedi@users.noreply.github.com>
2024-09-22 21:47:00 +0530

    first commit

---
+++ src/test/java/com/ot/onetimetoken/OnetimetokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ot.onetimetoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetimetokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
